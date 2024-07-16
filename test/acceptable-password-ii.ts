73f9665588c1199a1cf1b9d19ab9ddce06cdfe2b
jerry <piyapongvisitsin@gmail.com>
2024-07-16 08:53:54 +0700

    refactor

--- test/acceptable-password-ii.ts
+++ test/acceptable-password-ii.ts
@@ -1,9 +1,6 @@
 export class AcceptablePassword {
   validatePassword(password: string): boolean {
     let num = ['0','1','2','3','4','5','6','7','8','9'];
-    //password.split('').some((char) => num.includes(char));
-     console.log(num.includes(password));
-      
       if (password.length > 6 && password.split('').some((char) => num.includes(char))){
         return true;
       }
